3e3d21db989488913af36961625afe8f3164bac4
camilochaveztorres <93767257+camilochaveztorres@users.noreply.github.com>
2023-11-20 13:34:16 +0100

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Assignment_3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtCentimeter = new System.Windows.Forms.TextBox();
+            this.txtInch = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblNormalBMI = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnMetric = new System.Windows.Forms.RadioButton();
+            this.rbtnImperial = new System.Windows.Forms.RadioButton();
+            this.grpResults = new System.Windows.Forms.GroupBox();
+            this.lblWeightCat = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.grpResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(163, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(23, 87);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(58, 20);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height";
+            // 
+            // txtCentimeter
+            // 
+            this.txtCentimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCentimeter.Location = new System.Drawing.Point(163, 87);
+            this.txtCentimeter.Name = "txtCentimeter";
+            this.txtCentimeter.Size = new System.Drawing.Size(74, 27);
+            this.txtCentimeter.TabIndex = 3;
+            // 
+            // txtInch
+            // 
+            this.txtInch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInch.Location = new System.Drawing.Point(252, 87);
+            this.txtInch.Name = "txtInch";
+            this.txtInch.Size = new System.Drawing.Size(74, 27);
+            this.txtInch.TabIndex = 4;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(163, 192);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(163, 37);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "Calculate";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblNormalBMI
+            // 
+            this.lblNormalBMI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNormalBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNormalBMI.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblNormalBMI.Location = new System.Drawing.Point(154, 400);
+            this.lblNormalBMI.Name = "lblNormalBMI";
+            this.lblNormalBMI.Size = new System.Drawing.Size(266, 23);
+            this.lblNormalBMI.TabIndex = 10;
+            this.lblNormalBMI.Text = "label3";
+            this.lblNormalBMI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnImperial);
+            this.groupBox1.Controls.Add(this.rbtnMetric);
+            this.groupBox1.Location = new System.Drawing.Point(374, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Unit";
+            // 
+            // rbtnMetric
+            // 
+            this.rbtnMetric.AutoSize = true;
+            this.rbtnMetric.Location = new System.Drawing.Point(22, 34);
+            this.rbtnMetric.Name = "rbtnMetric";
+            this.rbtnMetric.Size = new System.Drawing.Size(114, 20);
+            this.rbtnMetric.TabIndex = 0;
+            this.rbtnMetric.TabStop = true;
+            this.rbtnMetric.Text = "Metric (kg, cm)";
+            this.rbtnMetric.UseVisualStyleBackColor = true;
+            this.rbtnMetric.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbtnImperial
+            // 
+            this.rbtnImperial.AutoSize = true;
+            this.rbtnImperial.Location = new System.Drawing.Point(22, 60);
+            this.rbtnImperial.Name = "rbtnImperial";
+            this.rbtnImperial.Size = new System.Drawing.Size(117, 20);
+            this.rbtnImperial.TabIndex = 1;
+            this.rbtnImperial.TabStop = true;
+            this.rbtnImperial.Text = "Imperial (ft, lbs)";
+            this.rbtnImperial.UseVisualStyleBackColor = true;
+            this.rbtnImperial.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // grpResults
+            // 
+            this.grpResults.Controls.Add(this.lblWeightCat);
+            this.grpResults.Controls.Add(this.label4);
+            this.grpResults.Controls.Add(this.lblBMI);
+            this.grpResults.Controls.Add(this.label2);
+            this.grpResults.Location = new System.Drawing.Point(26, 235);
+            this.grpResults.Name = "grpResults";
+            this.grpResults.Size = new System.Drawing.Size(361, 142);
+            this.grpResults.TabIndex = 12;
+            this.grpResults.TabStop = false;
+            this.grpResults.Text = "Results for";
+            this.grpResults.Enter += new System.EventHandler(this.grpResults_Enter);
+            // 
+            // lblWeightCat
+            // 
+            this.lblWeightCat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWeightCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeightCat.Location = new System.Drawing.Point(211, 92);
+            this.lblWeightCat.Name = "lblWeightCat";
+            this.lblWeightCat.Size = new System.Drawing.Size(100, 23);
+            this.lblWeightCat.TabIndex = 13;
+            this.lblWeightCat.Text = "label3";
+            this.lblWeightCat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Weight Category";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(211, 26);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(100, 23);
+            this.lblBMI.TabIndex = 11;
+            this.lblBMI.Text = "label3";
+            this.lblBMI.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Your BMI";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeight.Location = new System.Drawing.Point(23, 136);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(61, 20);
+            this.lblWeight.TabIndex = 13;
+            this.lblWeight.Text = "Weight";
+            this.lblWeight.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 22);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(252, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 22);
+            this.textBox2.TabIndex = 15;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.grpResults);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblNormalBMI);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtInch);
+            this.Controls.Add(this.txtCentimeter);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpResults.ResumeLayout(false);
+            this.grpResults.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtCentimeter;
+        private System.Windows.Forms.TextBox txtInch;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblNormalBMI;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnMetric;
+        private System.Windows.Forms.RadioButton rbtnImperial;
+        private System.Windows.Forms.GroupBox grpResults;
+        private System.Windows.Forms.Label lblWeightCat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_3
+{
+    public partial class MainForm : Form
+    {
+        private string name = string.Empty;
+        public MainForm()
+        {
+            InitializeComponent();
+            //Begin my work
+
+            InitializeGUI();
+          
+        }
+
+        private void InitializeGUI()
+        {
+            this.Text += " Camilo Chavez";
+            lblBMI.Text = string.Empty;
+            lblWeightCat.Text = string.Empty;
+            lblNormalBMI.Text = "Normal Weight";
+            rbtnMetric.Checked = true;
+
+        }
+
+        private void UpdateHeightText()
+        {
+            if (rbtnMetric.Checked )
+            {
+                lblHeight.Text = "Height (cm)";
+                lblWeight.Text = "Weight (kg)";
+            }
+            else 
+            { 
+                lblHeight.Text = "Height (ft, in)";
+                lblWeight.Text = "Weight (lbls)";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateHeightText();
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            UpdateHeightText();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpResults_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // 1 read input
+            // 2 Calculate
+            // 3 show results
+            name = txtName.Text.Trim();
+            grpResults.Text = name;
+
+            double height = 0.0;
+            double weight = 0.0;
+
+            //try parse
+
+            bool ok = double.TryParse(txtCentimeter.Text, out height);
+            if ( !ok )
+            {
+                MessageBox.Show("The height value is invalid.", "Error!");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
